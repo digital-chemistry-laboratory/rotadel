fdc89f7fc2931b1fcceaff413725c0aa4437ea40
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2025-03-28 13:56:49 +0100

    Improve output (json or print out) and error (rotamer ID printed)

--- gen_lib/main_one_rotamer.py
+++ gen_lib/main_one_rotamer.py
@@ -3,6 +3,9 @@ from os import PathLike
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from contextlib import nullcontext
+import sys
+import traceback
+import json
 
 from gen_lib.utils import extract_file_from_zip, lock
 from gen_lib.rotamer import Rotamer
@@ -49,69 +52,75 @@ def main(
     xyz_zip: PathLike | str,
     run_path: PathLike | str | None = None,
     output_json: PathLike | str | None = None,
-):  # TODO: add type hint for return
+) -> None:
     """Optimise rotamer geometry and calculate descriptors on it.
     Args:
         rotamer_id: ID of the rotamer
         angles_json: json file with the angles extracted from the Dunbrack library
         xyz_zip: path to the zipped xyz folder
         run_path: path of folder in which to perform the full optimisation process
-        output_json: optional path for output JSON file.
-            If a folder, 'data.json' will be created inside.
-            If not specified, './data.json' will be used.
+        output_json: path for output JSON file (if not provided, prints to stdout)
+    Returns:
+        None, writes data to prodided json file or stdout
     """
 
-    if output_json is None:
-        output_json = Path("./data.json")
-        if output_json.exists():
-            output_json.unlink()
-    else:
+    if output_json is not None:
         output_json = Path(output_json)
         if output_json.is_dir():
-            output_json = output_json / "data.json"
-            if output_json.exists():
-                output_json.unlink()
+            raise IsADirectoryError(
+                f"Provided path {output_json} is a directory. Please provide a file path."
+            )
+        elif output_json.exists():
+            raise FileExistsError(
+                f"Provided path {output_json} already exists. "
+                f"Please provide a different file name to avoid overwriting."
+            )
+
+    try:
+        rotamer = Rotamer(rotamer_id)
+
+        temp_dir_context = TemporaryDirectory() if run_path is None else nullcontext()
+        with temp_dir_context as temp_dir:
+            run_folder = Path(temp_dir) if run_path is None else Path(run_path)
+            if not run_folder.exists():
+                run_folder.mkdir(parents=True)
+
+            # Extract non-optimised rotamer xyz file
+            name_folder = Path(xyz_zip).stem
+            pre_opt_xyz = run_folder / f"{rotamer_id}_pre_opt.xyz"
+            extract_file_from_zip(
+                zip_archive=xyz_zip,
+                file_in_zip=f"{name_folder}/{rotamer_id}.xyz",
+                output_path=pre_opt_xyz,
+            )
+
+            # Optimise both whole rotamer and sidechain-H
+            el_whole, coord_whole, el_sidechain, coord_sidechain = get_opt_structures(
+                rotamer_key=rotamer_id,
+                run_folder=run_folder,
+                rotamer_xyz=pre_opt_xyz,
+                angles_json=angles_json,
+            )
+            rotamer.set_opt_geometries(
+                el_whole, coord_whole, el_sidechain, coord_sidechain
+            )
+
+        # Calculate descriptors
+        descriptors = calc_descriptors(el_sidechain, coord_sidechain)
+        rotamer.descriptors = descriptors
+
+        # Save as json
+        if output_json:
+            with lock(output_json):
+                rotamer.to_json(output_json)
         else:
-            if output_json.exists():
-                raise FileExistsError(
-                    f"JSON file for output already exists. Exit to avoid overwriting.\nProvided path: {output_json}"
-                )
+            sys.stdout.write(json.dumps(rotamer.to_dict(), indent=4))
 
-    rotamer = Rotamer(rotamer_id)
-
-    temp_dir_context = TemporaryDirectory() if run_path is None else nullcontext()
-    with temp_dir_context as temp_dir:
-        run_folder = Path(temp_dir) if run_path is None else Path(run_path)
-        if not run_folder.exists():
-            run_folder.mkdir(parents=True)
-
-        # Extract non-optimised rotamer xyz file
-        name_folder = Path(xyz_zip).stem
-        pre_opt_xyz = run_folder / f"{rotamer_id}_pre_opt.xyz"
-        extract_file_from_zip(
-            zip_archive=xyz_zip,
-            file_in_zip=f"{name_folder}/{rotamer_id}.xyz",
-            output_path=pre_opt_xyz,
-        )
-
-        # Optimise both whole rotamer and sidechain-H
-        el_whole, coord_whole, el_sidechain, coord_sidechain = get_opt_structures(
-            rotamer_key=rotamer_id,
-            run_folder=run_folder,
-            rotamer_xyz=pre_opt_xyz,
-            angles_json=angles_json,
-        )
-        rotamer.set_opt_geometries(el_whole, coord_whole, el_sidechain, coord_sidechain)
-
-    # Calculate descriptors
-    descriptors = calc_descriptors(el_sidechain, coord_sidechain)
-    rotamer.descriptors = descriptors
-
-    # Save as json
-    with lock(output_json):
-        rotamer.to_json(output_json)
+    except Exception:
+        # raise # Uncomment to see the error in the terminal as usual
+        sys.stderr.write(f"*** Rotamer {rotamer_id}\n{traceback.format_exc()}\n")
 
 
 if __name__ == "__main__":
     rotamer_id, angles_json, xyz_zip, output_json = parse_args()
-    main(rotamer_id, angles_json, xyz_zip, output_json)
+    main(rotamer_id, angles_json, xyz_zip, output_json=output_json)
