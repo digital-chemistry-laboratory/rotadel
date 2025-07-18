825e0309e8f73bdfac8e0d162edbb40fe0239533
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2025-07-18 12:03:02 +0200

    feat: possibility to save results either with individual json outputs in given folder, or in same given json file, or print to stdout

--- gen_lib/main_one_rotamer.py
+++ gen_lib/main_one_rotamer.py
@@ -30,8 +30,8 @@ def parse_args():
         "-o",
         "--output_json",
         type=Path,
-        help="optional path for output JSON file. If a folder, 'data.json' will be created inside. \
-            If not specified, './data.json' will be used.",
+        help="optional path for output JSON file. If folder, individual rotamer_id.json will be created inside. \
+            If not specified, output will be printed to stdout.",
         default=None,
     )
     args = parser.parse_args()
@@ -50,17 +50,16 @@ def main(
         rotamer_id: ID of the rotamer with species information
         angles_json: json file with the angles extracted from the Dunbrack library
         run_path: path of folder in which to perform the full optimisation process
-        output_json: path for output JSON file (if not provided, prints to stdout)
+        output_json: path for output JSON file
+            If json file, write rotamer output to it.
+            If folder, create individual rotamer_id.json inside.
+            If None, print output to stdout.
     Returns:
-        None, writes data to prodided json file or stdout
+        None, writes data to json file or stdout
     """
 
     if output_json is not None:
         output_json = Path(output_json)
-        if output_json.is_dir():
-            raise IsADirectoryError(
-                f"Provided path {output_json} must be a file not a directory."
-            )
 
     with open(angles_json, "r") as f:
         dunbrack_all_rotamers = json.load(f)
@@ -83,27 +82,48 @@ def main(
                 rotamer_id, dunbrack_data, charge, tautomer, run_path=run_folder
             )
 
-            already_calculated = False
-            if output_json and output_json.exists():
+            rotamer_already_exists = False
+            sidechain_already_calculated = False
+            if output_json and output_json.exists() and output_json.is_file():
                 with lock(output_json):
-                    already_calculated = rotamer.load_existing_sidechain(output_json)
+                    with open(output_json, "r") as f:
+                        content = json.load(f)
+                    rotamer_already_exists = rotamer_id in content
 
-            if not already_calculated:
-                if not run_folder.exists():
-                    run_folder.mkdir(parents=True)
+                    if not rotamer_already_exists:
+                        sidechain_already_calculated = rotamer.load_existing_sidechain(
+                            output_json
+                        )
 
-                # Optimise both whole rotamer and sidechain-H
-                rotamer.opt_geometries()
+            if not rotamer_already_exists:
+                if not sidechain_already_calculated:
+                    if not run_folder.exists():
+                        run_folder.mkdir(parents=True)
 
-                # Calculate descriptors on sidechain-H
-                rotamer.calc_descriptors()
+                    # Optimise both whole rotamer and sidechain-H
+                    rotamer.opt_geometries()
 
-                # Save as json
-                if output_json:
+                    # Calculate descriptors on sidechain-H
+                    rotamer.calc_descriptors()
+
+                # Save as json or print out
+                if output_json is None:
+                    sys.stdout.write(json.dumps(rotamer.to_dict(), indent=4))
+                elif output_json.suffix == ".json":
                     with lock(output_json):
                         rotamer.to_json(output_json)
+                elif output_json.suffix == "":
+                    if not output_json.exists():
+                        output_json.mkdir(parents=True)
+                    individual_output_json = output_json / f"{rotamer_id}.json"
+                    if individual_output_json.exists():
+                        individual_output_json.unlink()
+                    rotamer.to_json(individual_output_json)
                 else:
-                    sys.stdout.write(json.dumps(rotamer.to_dict(), indent=4))
+                    raise ValueError(
+                        f"The given argument `output_json` must be a file with .json suffix or be a folder. "
+                        f"You gave: {output_json}"
+                    )
 
     except Exception:
         # raise # Uncomment to see the error in the terminal as usual
