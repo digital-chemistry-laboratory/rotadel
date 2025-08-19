ae81cd641f3578593a1ef65946105a74328faa05
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2025-08-19 14:06:11 +0200

    ops: scripts to run rotamer calculations in series

---
+++ gen_lib/template_submit_batches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to submit the submitting script for each dataset batch
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+
+for batch_file in data/batches_keys_species_9000_filtered/batch_*; do
+  sbatch --export=BATCH_FILE=${batch_file} submit_series.sh
+done
---
+++ gen_lib/template_submit_series.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --mem-per-cpu=2GB
+#SBATCH -A es_jorner
+#SBATCH -t 48:00:00
+#SBATCH -J series_rot
+
+start_time=$(date +%s)
+
+# Set up environment
+source activate /cluster/project/jorner/lajacot/miniforge3/envs/aa
+export MY_MODULEPATH_ROOT=/cluster/project/jorner/modules_dcl/
+module use $MY_MODULEPATH_ROOT/Core
+module load xtb/bleed
+export PYTHONPATH=/cluster/project/jorner/lajacot/miniforge3/envs/aa/lib/python3.11/site-packages:$PYTHONPATH
+export PYTHONPATH=/cluster/project/jorner/lajacot/projects/aa-descriptors-library:$PYTHONPATH
+
+# Files paths
+input_file="${BATCH_FILE}"
+batch_number=$(basename "${input_file}" | sed 's/\.[^.]*$//' | awk -F'_' '{print $NF}')
+output_db="data/batches_sql_output/rotamers_sql_${batch_number}.db"
+angles_json="data/angles_Dunbrack.json"
+
+# Run
+echo "--- Run for ${input_file} ---"
+while IFS= read -r line || [ -n "$line" ]; do
+    python main_one_rotamer.py $line ${angles_json} -o ${output_db}
+done < ${input_file}
+
+# Calculate total run time
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+echo -e "\nTotal execution time: $((duration/3600))h $(((duration%3600)/60))m $((duration%60))s"
