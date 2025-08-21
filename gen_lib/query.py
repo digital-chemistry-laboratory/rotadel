9ab984b6f5d15a54d3d06114f490f931148caffc
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2025-08-21 17:00:51 +0200

    feat: calculate rmsd without hydrogens, return dictionary of descriptors

--- gen_lib/query.py
+++ gen_lib/query.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sqlite3
+import json
 from morfeus import read_xyz
 from spyrmsd.rmsd import rmsd
 
@@ -18,33 +19,57 @@ def query_target(
     tautomer: str | None = None,
     sql_path: str | Path | None = None,
 ):
+    """Query the rotamer in the library whose side-chain is the closest (ignoring hydrogens) to the given structure
+    Args:
+        target_rotamer_xyz: xyz file of the target rotamer
+        residue: amino acid type of the target rotamer
+        charge: charge of the target rotamer
+        tautomer: tautomer of the target rotamer if applicable
+        sql_path: path to the SQL database file
+    Returns:
+        Dictionary with ID, RMSD, and descriptors of the closest rotamer found in the library
+    """
     if sql_path is None:
         sql_path = SQL_PATH
 
     el_target, coords_target = read_xyz(target_rotamer_xyz)
-    closest = {"rotamer_id": None, "rmsd": float("inf")}
+    # Remove hydrogens
+    mask_noHs = el_target != "H"
+    el_target_noHs = el_target[mask_noHs]
+    coords_target_noHs = coords_target[mask_noHs]
+
+    closest_rot = {"rotamer_id": None, "rmsd": float("inf"), "descriptors": None}
 
     with sqlite3.connect(sql_path) as conn:
         cur = conn.cursor()
         cur.execute(
-            "SELECT rotamer_id FROM rotamers_data where res = ? AND charge = ? AND tautomer IS ?",
+            "SELECT rotamer_id, descriptors FROM rotamers_data where res = ? AND charge = ? AND tautomer IS ?",
             (residue, charge, tautomer),
         )
-        for (rotamer_id,) in cur.fetchall():
+        for rotamer_id, descriptors in cur.fetchall():
             rotamer_xyz = get_xyz_from_sql(cur, "sidechainH_xyz", rotamer_id)
             el_rotamer, coords_rotamer = (
                 rotamer_xyz["elements"],
                 rotamer_xyz["coordinates"],
             )
+
+            # Remove hydrogens
+            el_rotamer_noHs = el_rotamer[mask_noHs]
+            coords_rotamer_noHs = coords_rotamer[mask_noHs]
+
             rmsd_val = rmsd(
-                coords_target,
-                coords_rotamer,
-                el_target,
-                el_rotamer,
+                coords_target_noHs,
+                coords_rotamer_noHs,
+                el_target_noHs,
+                el_rotamer_noHs,
                 center=True,
                 minimize=True,
             )
-            if rmsd_val < closest["rmsd"]:
-                closest = {"rotamer_id": rotamer_id, "rmsd": rmsd_val}
+            if rmsd_val < closest_rot["rmsd"]:
+                closest_rot = {
+                    "rotamer_id": rotamer_id,
+                    "rmsd": rmsd_val,
+                    "descriptors": json.loads(descriptors),
+                }
 
-    return closest
+    return closest_rot
