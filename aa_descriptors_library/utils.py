ad099a53fabde89870686c6a48da92d4c6fb7a41
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2026-01-26 13:00:31 +0100

    feat: function to calculate rmsd taking into account different atom ordering

--- aa_descriptors_library/utils.py
+++ aa_descriptors_library/utils.py
@@ -1,11 +1,15 @@
-from os import PathLike
-from pathlib import Path
 import contextlib
 import fcntl
-import zipfile
+from os import PathLike
+from pathlib import Path
 import shutil
-import numpy as np
+import zipfile
+
+from morfeus import read_xyz
 from morfeus.typing import Array1DStr, Array2DFloat
+from morfeus.utils import convert_elements
+import numpy as np
+from spyrmsd import rmsd, graph
 
 
 @contextlib.contextmanager
@@ -141,8 +145,7 @@ def get_atom_count(xyz_file):
 
 
 def xyz_string(elements: Array1DStr, coordinates: Array2DFloat) -> str:
-    """
-    Make a XYZ string from elements and coordinates.
+    """Make a XYZ string from elements and coordinates.
     Args:
         elements: elements symbols
         coordinates: coordinates [Å]
@@ -154,3 +157,41 @@ def xyz_string(elements: Array1DStr, coordinates: Array2DFloat) -> str:
     for element, coords in zip(elements, coordinates):
         xyz_string += f"{element} {coords[0]} {coords[1]} {coords[2]}\n"
     return xyz_string
+
+
+def rmsd_symmetric(
+    xyz_file1: str | Path | None = None,
+    xyz_file2: str | Path | None = None,
+    el1: Array1DStr | None = None,
+    coords1: Array2DFloat | None = None,
+    el2: Array1DStr | None = None,
+    coords2: Array2DFloat | None = None,
+    diff_order: bool = True,
+) -> float:
+    """Calculate RMSD between two molecular structures, handling different atom ordering.
+    Args:
+        xyz_file1, xyz_file2: paths to XYZ files
+        el1, el2: elements
+        coords1, coords2: coordinates [Å]
+        diff_order: whether atoms are in different order
+    Returns:
+        RMSD value [Å]
+    """
+
+    if xyz_file1 is not None:
+        el1, coords1 = read_xyz(xyz_file1)
+    if xyz_file2 is not None:
+        el2, coords2 = read_xyz(xyz_file2)
+
+    if diff_order:
+        el_nums1 = convert_elements(el1)
+        el_nums2 = convert_elements(el2)
+        adj1 = graph.adjacency_matrix_from_atomic_coordinates(el_nums1, coords1)
+        adj2 = graph.adjacency_matrix_from_atomic_coordinates(el_nums2, coords2)
+        rmsd_value = rmsd.symmrmsd(
+            coords1, coords2, el_nums1, el_nums2, adj1, adj2, center=True, minimize=True
+        )
+    else:
+        rmsd_value = rmsd.rmsd(coords1, coords2, el1, el2, center=True, minimize=True)
+
+    return rmsd_value
