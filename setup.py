8e9f25d055340b7eea82d5d82021656e91879f28
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2026-03-27 00:14:09 +0100

    build: switch from setup.py to recommended pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="aa_descriptors_library",
-    author="Lauriane Jacot-Descombes",
-    version="0.0.0",
-    packages=find_packages(),
-)
